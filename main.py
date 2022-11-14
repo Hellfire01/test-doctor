5ce62d2a5221652cf0fa204cbe87652c2fa2bead
Mat RF <matthieu.raynaud-de-fitte@outlook.com>
2022-11-14 18:40:32 +0100

    preparation

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ def main():
     # This is the cli usage but you can call the test doctor directly should you wish to integrate it
     test_doctor_cli = TestDoctorCLI()
     instructions = test_doctor_cli.get_instructions(sys.argv)
-    test_doctor = TestDoctor()
+    test_doctor = TestDoctor(instructions.report_contents,
+                             instructions.root,
+                             instructions.disable_auto_root,
+                             instructions.slow_test_threshold,
+                             instructions.save_graph_path,
+                             instructions.disable_show_graph)
 
 
 if __name__ == "__main__":
--- src/test_doctor.py
+++ src/test_doctor.py
@@ -1,8 +1,14 @@
 
 
 class TestDoctor:
-    def __init__(self):
-        pass
+    def __init__(self, file_content: str, root: str, disable_auto_root: bool, slow_test_threshold: float,
+                 save_graph_path: str, disable_show_graph: bool):
+        self.file_content = file_content
+        self.root = root
+        self.disable_auto_root = disable_auto_root
+        self.slow_test_threshold = slow_test_threshold
+        self.disable_show_graph = disable_show_graph
+        self.save_graph_path = save_graph_path
 
     def analyse(self):
         # check received instructions
